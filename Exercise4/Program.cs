af33f0ee4d4b9c35a50293a987c5cb56ba8cc3ce
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2022-12-07 16:11:40 +0700

    add method pop

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -32,6 +32,12 @@ namespace Exercise4
             else
                 return (false);
         }
+        public void pop()
+        {
+            Console.WriteLine("\nThe popped elements is: " + top.ponco);
+            top = top.next;
+        }
+
         public void push(char[] element)
         {
             Node fresh;
@@ -41,7 +47,54 @@ namespace Exercise4
             top = fresh;
             Console.WriteLine("\n" + element + " pushed.");
         }
-        public void pop()
 
+        public void display()
+        {
+            Node tmp;
+            if (empty())
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.ponco);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Pop");
+                Console.WriteLine("2. Push");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.Write("\nEnter your choice: ");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                switch (ch)
+                {
+                    case '1':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
+                    case '2':
+                        Console.Write("\nEnter a number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.Push(num);
+                        break;
+                    case '3':
+                        s.display()
+                }
+            }
+        }
     }
 }
